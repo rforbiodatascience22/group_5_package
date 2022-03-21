5089f20f4d74054860969f9d1a05592443ad8930
nilshof01 <s220672@dtu.dk>
2022-03-21 08:48:02 +0000

    codons

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Codon Table
+#'
+#' Table of DNA codons to amino acids
+#'
+#'
+#' @format Vector of 64 codons with the corresponding amino acid
+#'
+#' @source \url{https://learn.inside.dtu.dk/content/enforced/102974-OFFERING-653081/lab08/lab_08_r_package_exercises.html}
+"codon_table"

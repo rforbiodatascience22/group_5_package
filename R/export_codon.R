975444d0a1a9881ede6c88ccc1f32b98b6dc5e7b
nilshof01 <s220672@dtu.dk>
2022-03-21 09:51:33 +0000

    jjjjj

--- R/export_codon.R
+++ R/export_codon.R
@@ -1,14 +1,21 @@
 
 #' Title
-#'export Codon
+#'export amino_acid
 #' @param codons
 #'
-#' @return codon string with aminoacid and codon
+#' @return amino_acid
 #' @export
 #'
 #' @examples
+#'paste0(codon_table['AGU'], collapse = "")
+#'"S"
 #'
-export_codon <- function(codons){
+export_amino_acid <- function(codons){
   codon_string <- paste0(codon_table[codons], collapse = "")# collpse converts multiple strings into one character string
   return(codont_string)
 }
+
+
+export_amino_acid("ABC")
+export_amino_acid("AUGU")
+export_amino_acid("AUG")
